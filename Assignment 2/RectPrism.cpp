05a70ca03887972bbb8e72606a45b6f686fe2382
katherine-yuan <katherine.yuan98@gmail.com>
2018-08-28 22:55:40 +1000

    Add project files.

---
+++ Assignment 2/RectPrism.cpp
@@ -0,0 +1,18 @@
+#include "Shape.hpp"
+#include "RectPrism.h"
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#elif defined(WIN32)
+#include <Windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#else
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#endif
+
---
+++ Assignment 2/RectPrism.h
@@ -0,0 +1,30 @@
+#ifndef MTRN3500_RECTPRISM_H
+#define MTRN3500_RECTPRISM_H
+
+#include "Shape.hpp"
+
+class RectPrism : public Shape {
+
+public:
+	RectPrism(); //default constructor
+	RectPrism(double x_, double y_, double z_, double rotation_, double xLength_, double yLength_, double zLength_);
+	~RectPrism();
+
+	//Getter functions
+	double getXLength();
+	double getYLength();
+	double getZLength();
+
+	//Setter functions
+	void setXLength(double xLength_);
+	void setYLength(double yLength_);
+	void setZLength(double zLength_);
+
+	//Draw function
+	void draw();
+
+protected:
+	double xLength, yLength, zLength;
+};
+
+#endif // for MTRN3500_RECTPRISM_H
